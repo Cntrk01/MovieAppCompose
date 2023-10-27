c7798212c33cb38dcb2c0e5a5b47ccd555df8d89
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2023-10-27 14:01:24 +0300

    Update
    Burada main ekran için error ve loading durumlarını ekledım.TextFieldin boş olması durumunda default olarak Batman querysini atadım

--- app/src/main/java/com/compose/movieappcompose/presentation/movie/views/MovieScreen.kt
+++ app/src/main/java/com/compose/movieappcompose/presentation/movie/views/MovieScreen.kt
@@ -11,14 +11,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
@@ -51,6 +55,7 @@ fun MovieScreen(
                 onSearch = {
                     viewModel.onEvent(MoviesEvent.Search(it))
                 })
+
             LazyColumn(modifier = Modifier.fillMaxSize()){
                 items(state.movies){movie->
                     MovieListRow(movie = movie, onItemClick = {
@@ -58,7 +63,21 @@ fun MovieScreen(
                         navController.navigate(Screen.MovieDetailScreen.route+"/${movie.imdbID}")
                     })
                 }
+
             }
+        }
+        if (state.error.isNotBlank()){
+            Text(text = state.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign= TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading){
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
 }
@@ -74,33 +93,43 @@ fun MovieSearchBar(
         mutableStateOf("")
     }
     var isHintDisplay by remember {
-        //boş olmadığında hint gösterilecek
+        //eğer hintin bir değeri varsa true dönücek
         mutableStateOf(hint !="")
     }
     Box(modifier = modifier){
         TextField(value = text, onValueChange = {
             text=it
+            //Burada eklediğim kodla boş olduğu an tekrar Batmani getirmesini sağladım.
+            if (it.isEmpty()){
+                onSearch.invoke("Batman")
+            }
         },//burada kullanıcı klavyede yazıp okey tusuna bastıgında onSearch methoduna yazılan stringi veriyoruz onuda yukarda viewmodele gönderiyoruz
             keyboardActions = KeyboardActions(onDone = {//burada unit dönüyor yani kullanıcı okey tusuna bastıgında içinde olmasını istediğimiz işlemleri yapıyoruz.
                 onSearch(text)
+                //query boş olfugunda okeylediğinde batman yazdırıyor
+                if (text.isEmpty()){
+                    onSearch("Batman")
+                }
             }), maxLines = 1,
                 singleLine = true,
                 textStyle = TextStyle(color = Color.Black),
-                shape = RoundedCornerShape(12.dp),
+                shape = RoundedCornerShape(3.dp),
+                colors = TextFieldDefaults.textFieldColors(Color.White),
                 modifier= Modifier
                     .fillMaxWidth()
                     .shadow(5.dp, CircleShape)
                     .background(Color.White, CircleShape)
-                    .padding(20.dp)
+                    .padding(horizontal = 5.dp)
                     //bu method kullanıcı TextField e tıkladığı anda aktif oluyor.Buraya fonksiyondan parametre veriyoruz.Hint geldiği için true dönüyor.
                     .onFocusChanged {
+                        //kullanıcı searchbara tıkladığı anda isHintDisplay false dönücek
                         isHintDisplay = it.isFocused != true && text.isEmpty()
                     }
         )
-//        if (isHintDisplay){
-//            Text(text = hint,
-//                color = Color.LightGray,
-//                modifier=Modifier.padding(horizontal = 20.dp, vertical = 12.dp))
-//        }
+        if (isHintDisplay){
+            Text(text = hint,
+                color = Color.Black,
+                modifier=Modifier.padding(horizontal = 20.dp, vertical = 12.dp).align(Alignment.CenterStart))
+        }
     }
 }
