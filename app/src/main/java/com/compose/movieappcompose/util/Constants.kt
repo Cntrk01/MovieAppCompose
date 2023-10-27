c32894fdfa404d6334108bb9c00174b43c8c39d5
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2023-10-27 14:04:32 +0300

    Update Constants.kt

--- app/src/main/java/com/compose/movieappcompose/util/Constants.kt
+++ app/src/main/java/com/compose/movieappcompose/util/Constants.kt
@@ -2,6 +2,6 @@ package com.compose.movieappcompose.util
 
 object Constants{
     const val BASE_URL="http://www.omdbapi.com"
-    const val API_KEY="4d0dbb24"
+    const val API_KEY="YOUR KEY"
     const val IMDB_ID="imdb_id"
 }
